001c2d8b4b7b509ea67716ce8b32c593f0b06011
Antonela Rakipaj <nelarakipaj@gmail.com>
2025-03-21 10:11:31 +0000

    100 days of challenge

---
+++ contents/Day 1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is your pet name? ")
+print("Your band name could be " + city + "  " + pet)
