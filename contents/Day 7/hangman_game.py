58ae5503e979b9b992bdcd35cabe31b074594f1d
Antonela Rakipaj <nelarakipaj@gmail.com>
2025-03-27 22:36:05 +0000

    day 7 - completed

--- contents/Day 7/hangman_game.py
+++ contents/Day 7/hangman_game.py
@@ -1,14 +1,34 @@
 import random
-word_list = ["hardwork","baboon","camel"]
+from ascii_codes import stages
+from list_of_words import word_list
 
+print("""                 Welcome to the best Hangman Game
+            ~Done by the precious 'Antonela Rakipaj'
+        -------🤖--------Enjoy the game!!---------🤖-----""")
 word = random.choice(word_list)
+display = list("_" * len(word))
 
-answer = (input("Guess a letter:")).lower
-print(type(word),type(answer) )
-for letter in word:
-    if letter == answer:
-        print("wrong")
-    elif letter != answer:
-        print (letter)
-        print("correct")
+output = " ".join(display)
+print(f"The word is: {output}")
+lives = 6
 
+while lives > 0:
+    answer = input("Guess a letter:").lower()
+    if answer in word:  
+        for position in range(len(word)):
+            if word[position] == answer:
+                display[position] = answer
+        output = " ".join(display)
+        print(output)
+    else:
+        lives -= 1
+        print(f"Meh, {lives} lives left")
+        print(stages[lives])
+        
+        
+    if "_" not in display:
+        print("*******************🥇WINNER***************************\n")
+        game_over = True
+
+print("*********************😿  DISSAPOINTED**************************\n")
+print(f"The word was {word}. Maybe next time!\n")
