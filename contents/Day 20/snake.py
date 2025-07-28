47ea2cddfc5a45489e1da25f63ffed420e656da7
Antonela Rakipaj <nelarakipaj@gmail.com>
2025-07-28 19:02:01 +0100

    Implement Snake game logic with class structure and movement controls

---
+++ contents/Day 20/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]       # keep track of the “head”
+
+    def create_snake(self):
+        x_pos = 0
+        for _ in range(3):
+            segment = Turtle("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(x_pos, 0)
+            self.segments.append(segment)
+            x_pos -= MOVE_DISTANCE
+
+    def move(self):
+        # move each segment to the position of the one ahead of it
+        for i in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[i-1].xcor()
+            new_y = self.segments[i-1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        # finally move the head forward
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
--- contents/Day 20/snake_game.py
+++ contents/Day 20/snake_game.py
@@ -1,48 +1,26 @@
-from turtle import Screen, Turtle
-#to see each of the updates with delay 
+from turtle import Screen
+from snake import Snake
 import time
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Nokia 3310 - Snake Game")
 screen.tracer(0)
-x_pos = 0
-segments = []
-for _ in range(0, 3):
-    segment_1 = Turtle("square")
-    segment_1.penup()
-    segment_1.color("white")
-    segment_1.goto(x_pos,0)
-    x_pos-=20
-    segments.append(segment_1)
 
-#to freeze the whole body
-screen.update()
-game_is_on = True 
+snake = Snake()
 
+# set up key bindings
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
-    for seg in range(len(segments)-1, 0, -1):
-    #explanation: will be that 3rd will go to 2nd and 2nd will go to 1st and will turn
-        new_x = segments[seg-1].xcor() #second to last segment position
-        new_y = segments[seg-1].ycor() #get hold of y
-        segments[seg].goto(new_x, new_y) #3st 
-        #telling the last segment to go to the position of 1 before segment 
-    #get hold of 1st segment
-    segments[0].forward(20)
-    
-    
-    
-        
-    #the segments are not linked so we will need to change 
-    # for seg in segments:
-    #     seg.forward(20)
-    #segments[0].left[90] -> cannot be done like this 
-    
-    
-
-
+    snake.move()
 
 screen.exitonclick()
