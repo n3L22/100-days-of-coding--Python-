e51076363b2e82c2a8243d08123d3a301fec350b
Antonela Rakipaj <nelarakipaj@gmail.com>
2025-08-04 14:57:17 +0100

    Add personalized birthday invitation letters in DOCX format

--- contents/Day 23-Mail Merge/main.py
+++ contents/Day 23-Mail Merge/main.py
@@ -11,7 +11,7 @@ with open("100-days-of-coding--Python-\contents\Day 23-Mail Merge\Input\Letters\
                 
 for names in names_as_list:
         base_dir = "100-days-of-coding--Python-/contents/Day 23-Mail Merge/Output/ReadyToSend"
-        filename = f"letter_for_{names}.txt"  
+        filename = f"letter_for_{names}.docx"  
         personalized_letter = letter_template.replace("[name]", names)
         with open(f"{base_dir}/{filename}", "w") as file:  # Note the "w" mode
             file.write(personalized_letter)
