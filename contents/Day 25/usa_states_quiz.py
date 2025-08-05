9566188cdeca335e56dbe239289f8d46cd0e8131
Antonela Rakipaj <nelarakipaj@gmail.com>
2025-08-05 22:43:44 +0100

    Refactor U.S. States quiz logic for improved structure and user interaction

--- contents/Day 25/usa_states_quiz.py
+++ contents/Day 25/usa_states_quiz.py
@@ -1,15 +1,39 @@
-import turtle
+import turtle, pandas, time
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
+data = pandas.read_csv(r"100-days-of-coding--Python-\contents\Day 25\50_states.csv")
 image ="100-days-of-coding--Python-/contents/Day 25/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+t = turtle.Turtle()
+t.hideturtle()      
+message_turtle = turtle.Turtle()
+message_turtle.hideturtle()
+message_turtle.penup()
 
+correct_answer = []
+while len(correct_answer) < 50:
+    answer_state = screen.textinput(title=f"{len(correct_answer)}/50 Guess the State", prompt="What's another state's name?").title()
 
+    if answer_state in data["state"].values and answer_state not in correct_answer:
+        correct_answer.append(answer_state)
+        state_data = data[data.state == answer_state]
+        x = int(state_data.x)
+        y = int(state_data.y)
+        t.penup()             
+        t.goto(x,y)     
+        t.write(answer_state)
+    else:
+        message_turtle.goto(0,0) 
+        message_turtle.write("That's a duplicate!", align="center", font=("Arial", 16, "normal"))
+        time.sleep(2)
+        message_turtle.clear()
+
+message_turtle.goto(0,0) 
+message_turtle.write("YOU ARE SMART!!!!", align="center", font=("Arial", 16, "normal"))
 
 
 #to get the exact coordinates 
