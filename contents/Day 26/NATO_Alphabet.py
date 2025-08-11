40a2de7c902fbba3f4008275a8d3948fe9961886
Antonela Rakipaj <nelarakipaj@gmail.com>
2025-08-11 13:27:53 +0100

    Fix dictionary key access in NATO alphabet mapping

--- contents/Day 26/NATO_Alphabet.py
+++ contents/Day 26/NATO_Alphabet.py
@@ -2,7 +2,7 @@ import pandas
 
 data = pandas.read_csv(r"100-days-of-coding--Python-\contents\Day 26\nato_phonetic_alphabet.csv")
 
-data_to_dict = {row["letter"]:row["code"] for (index, row) in data.iterrows()}
+data_to_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 username = input("Enter a word: ").upper()
 
